eda5058d7c5b33a873cb08db01fdb8e77b7bfdcb
Belen Lopez <belennlopezvega@gmail.com>
2022-03-24 16:50:20 -0700

    Adding to the Earthquakes_past7days branch

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -12,6 +12,13 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     accessToken: API_KEY
 });
 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
@@ -30,7 +37,6 @@ let overlays = {
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
-
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
